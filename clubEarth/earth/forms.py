11e89eaf16f0e4dd43f7e0be388c74e1d3e45b7f
AlexanderKM <maniatesa@gmail.com>
2015-10-29 08:14:49 -0400

    completed register form display in index template

--- clubEarth/earth/forms.py
+++ clubEarth/earth/forms.py
@@ -12,6 +12,10 @@ class ProfileForm(forms.ModelForm):
 class UserForm(UserCreationForm):
 	username = forms.CharField(label="Username", required=True, 
 		widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username', }))
+	password1 = forms.CharField(label="Password", required=True,
+		widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+	password2 = forms.CharField(label="Confirm_Password", required=True,
+		widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
 	email = forms.EmailField(label='Email', required=True, 
 		widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
 
